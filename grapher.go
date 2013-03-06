802eaf6d866b26fa42bf80c4c047599f15280cd3
DeedleFake <yisszev@beckforce.com>
2013-03-06 15:20:17 -0500

    Draw axes.

--- grapher.go
+++ grapher.go
@@ -4,6 +4,7 @@ import (
 	"./graph"
 	"flag"
 	"github.com/DeedleFake/sdl"
+	"image"
 	"image/color"
 	"math"
 	"time"
@@ -13,14 +14,30 @@ func function(x float64) float64 {
 	return math.Log(x)
 }
 
+func drawAxes(d *Display) error {
+	err := d.Line(image.Pt(d.Width()/2, 0), image.Pt(d.Width()/2, d.Height()))
+	if err != nil {
+		return err
+	}
+
+	err = d.Line(image.Pt(0, d.Height()/2), image.Pt(d.Width(), d.Height()/2))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	var flags struct {
 		w, h int
 		p    float64
+		axes bool
 	}
 	flag.IntVar(&flags.w, "w", 640, "The width of the screen.")
 	flag.IntVar(&flags.h, "h", 480, "The height of the screen.")
 	flag.Float64Var(&flags.p, "p", .1, "The precision of the graph.")
+	flag.BoolVar(&flags.axes, "axes", true, "Draw axes.")
 	flag.Parse()
 
 	d, err := NewDisplay("Graph", flags.w, flags.h)
@@ -33,7 +50,19 @@ func main() {
 		panic(err)
 	}
 
-	err = d.Color(color.White)
+	if flags.axes {
+		err = d.Color(color.White)
+		if err != nil {
+			panic(err)
+		}
+
+		err = drawAxes(d)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = d.Color(color.RGBA{255, 0, 0, 0})
 	if err != nil {
 		panic(err)
 	}
